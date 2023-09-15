dc9c52e144cebb904c0893be65f108e76612b9d3
raviraja0969 <raviraja00979@gmail.com>
2023-09-15 06:17:23 +0000

    appending is in progress

--- installmysql.sh
+++ installmysql.sh
@@ -4,6 +4,24 @@ if [ $USERID -ne 0 ]
 then
     echo "ERROR:: Please run this script with root access"
     exit 1
-# else
-#     echo "INFO:: You are root user"
+else
+    echo "INFO:: You are root user"
+fi
+
+yum install mysql -y
+if [ $? -ne 0 ]
+then
+    echo "Installation of mysql is error"
+    exit 1
+else
+    echo "Installation of mysql is success"
+fi
+yum install postfix -y
+
+if [ $? -ne 0 ]
+then
+    echo "Installation of postfix is error"
+    exit 1
+else
+    echo "Installation of postfix is success"
 fi
