aac58b10f78e04d440b47beb28bf6b2c21df15d0
raviraja0969 <raviraja00979@gmail.com>
2023-09-15 12:23:33 +0530

    New file

---
+++ loops.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+DATE=$(date +%F)
+LOGSDIR=/home/centos/shellscript-logs
+# /home/centos/shellscript-logs/script-name-date.log
+SCRIPT_NAME=$0
+LOGFILE=$LOGSDIR/$0-$DATE.log
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+if [ $USERID -ne 0 ];
+then
+    echo -e "$R ERROR:: Please run this script with root access $N"
+    exit 1
+fi
